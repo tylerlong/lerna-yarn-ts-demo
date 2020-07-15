8f62a1d34092e15b47cc221b10a42d869691f285
Tyler Long <tyler4long@gmail.com>
2020-07-15 14:42:01 -0700

    Rewrite the tests

--- packages/tests/index.spec.ts
+++ packages/tests/index.spec.ts
@@ -3,9 +3,9 @@ import uniq2 from '@my-org/pkg2';
 
 describe('uniq', () => {
   test('pkg1', async () => {
-    console.log(uniq1([1, 2, 1, 2, 3]));
+    expect(uniq1([1, 2, 1, 2, 3])).toEqual([1, 2, 3]);
   });
   test('pkg2', async () => {
-    console.log(uniq2([1, 2, 1, 2, 3]));
+    expect(uniq2([1, 2, 1, 2, 3])).toEqual([1, 2, 3]);
   });
 });
